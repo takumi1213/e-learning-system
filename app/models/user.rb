53549af7e80248e8ef68dbcd2caedb5736b8c85d
takumi1213 <shinnsekai5@gmail.com>
2021-11-08 21:58:32 +0900

    signup

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ApplicationRecord
+  has_secure_password
+  validates :name, presence: true, length: { maximum: 50, minimum: 2}
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { mximum: 50, minimum: 4 },
+                                    format: { with: VALID_EMAIL_REGEX },
+                                    uniqueness: { case_sensitive: true }
+  
+  validates :password, length: { minimum: 6 }
+end
