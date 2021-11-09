7d9e29dd77bfe9627805f6a27dc4fc8ef67f3056
takumi1213 <91010905+takumi1213@users.noreply.github.com>
2021-11-09 22:23:42 +0900

    User profile (#4)
    * userprofile
    
    * fixed errors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,14 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, except: [:index ,:new, :create ,:update]
+  before_action :logged_in_user, except: [:index ,:new, :create, :show, :destroy]
   before_action :correct_user, only: :edit
+
+  def index
+    @users = User.paginate(page: params[:page], per_page: 10 )
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
 
   def new
     @user = User.new
@@ -29,11 +37,15 @@ class UsersController < ApplicationController
       render 'edit'
     end
   end
+  
+  def destroy
+    User.find(params[:id]).destroy
+    redirect_to users_url
+  end
 
   private
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
-    
   end
   def correct_user
     user = User.find(params[:id])
@@ -42,5 +54,4 @@ class UsersController < ApplicationController
       redirect_to root_url
      end
   end
-  
 end
