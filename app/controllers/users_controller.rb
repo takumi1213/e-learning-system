8eb149453e6235f2f2c27487bbef73111fbe4da3
takumi1213 <shinnsekai5@gmail.com>
2021-11-09 14:13:15 +0900

    create edit page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, except: [:index ,:new, :create]
+  before_action :logged_in_user, except: [:index ,:new, :create ,:update]
+  before_action :correct_user, only: :edit
 
   def new
     @user = User.new
@@ -15,10 +16,31 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:success] = "Profile updated!!"
+      redirect_to root_url
+    else
+      render 'edit'
+    end
+  end
+
   private
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
     
+  end
+  def correct_user
+    user = User.find(params[:id])
+     if user != current_user
+      flash[:danger] = "You're not authorized."
+      redirect_to root_url
+     end
   end
   
 end
