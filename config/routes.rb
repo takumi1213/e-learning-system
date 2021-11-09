c596f9326944dfa7596ac4850ef9517442e40979
takumi1213 <91010905+takumi1213@users.noreply.github.com>
2021-11-09 14:01:51 +0900

    login page (#2)

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   
+  get 'sessions/new'
   get 'static_pages/home'
   get 'static_pages/about'
   get 'static_pages/contact'
@@ -7,5 +8,10 @@ Rails.application.routes.draw do
   root  'static_pages#home'
   resources :users
   get '/signup', to: 'users#new' 
+
+  get '/login', to: 'sessions#new'
+  get '/logout', to: 'sessions#destroy'
+  resources :sessions, only: :create
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
