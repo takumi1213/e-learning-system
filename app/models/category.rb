978d7de44226bfaf5e931595da14950c4e0d30c5
takumi1213 <91010905+takumi1213@users.noreply.github.com>
2021-11-12 20:32:17 +0900

    create alert messages (#10)

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,4 @@
 class Category < ApplicationRecord
-  
+  validates :title, presence: true
+  validates :description, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   has_secure_password
   validates :name, presence: true, length: { maximum: 50, minimum: 2}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, length: { mximum: 50, minimum: 4 },
+  validates :email, presence: true, length: { maximum: 50, minimum: 4 },
                                     format: { with: VALID_EMAIL_REGEX },
                                     uniqueness: { case_sensitive: true }
   
