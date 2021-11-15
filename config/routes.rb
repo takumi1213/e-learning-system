bf1bf40d91abb4d2de8dc4a3fb89a1e7cc12b25e
takumi1213 <shinnsekai5@gmail.com>
2021-11-15 21:24:55 +0900

    editword

--- config/routes.rb
+++ config/routes.rb
@@ -17,7 +17,8 @@ Rails.application.routes.draw do
     resources :words
   end
 
-  resources :words, only: :create
+  resources :words, only: [:create,:update] 
 
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
