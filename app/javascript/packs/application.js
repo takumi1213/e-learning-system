9e7810715e8fd4d2a51f36ab0bbd9b1ec33c18e8
takumi1213 <shinnsekai5@gmail.com>
2021-11-15 22:02:47 +0900

    change js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,12 +4,11 @@
 // that code so it'll be compiled.
 
 import "bootstrap"
+import '@fortawesome/fontawesome-free/js/all'
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-
-import '@fortawesome/fontawesome-free/js/all'
 
 Rails.start()
 Turbolinks.start()
